10cd223f33967def201010862b2c040e7b20f308
emestabillo <emsestabillo@gmail.com>
2020-12-10 21:22:34 -0500

    Add searchmovies function

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,8 +1,25 @@
 import React from "react";
 
 function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+
+    const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=1e40b926a05f5f0a055187631b4f231e&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label htmlFor="query" className="label">
         Movie name
       </label>
